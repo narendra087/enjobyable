cdcc5ba033492244176a5809c6a80bc5a6262a11
Narendra <personal.rendra@gmail.com>
2023-06-26 16:51:16 +0700

    [ADD] Expo Router & Layout

---
+++ app/index.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import { Text, View, ScrollView, SafeAreaView } from 'react-native'
+import { Stack, useRouter } from 'expo-router'
+
+import { COLORS, SIZES, icons, images } from '../constants'
+
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+
+const Home = () => {
+  const router = useRouter()
+  
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Text>HAHAHA</Text>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: ""
+        }}
+      />
+      
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{flex:1, padding: SIZES.medium}}>
+          <Welcome />
+          
+          <Popularjobs />
+          
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+      
+    </SafeAreaView>
+  )
+}
+
+export default Home
